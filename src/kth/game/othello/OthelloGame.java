5057f9d47a54b0585357769ec6a0ce21eb86569c
AntonLovmar <antonlovmar@gmail.com>
2013-11-14 10:54:17 +0100

    Implemented more stuff

--- src/kth/game/othello/OthelloGame.java
+++ src/kth/game/othello/OthelloGame.java
@@ -2,19 +2,24 @@ package kth.game.othello;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import kth.game.othello.board.Board;
 import kth.game.othello.board.Node;
-import kth.game.othello.board.OthelloBoard;
+import kth.game.othello.board.OthelloNode;
 import kth.game.othello.player.Player;
 
 public class OthelloGame implements Othello{
 
 	private Board board;
 	private List<Player> players;
+	private Player playerInTurn;
+	private boolean active;
+	private int[] dX = {0, 0, 1, -1, 1, -1, -1, 1};
+	private int[] dY = {1, -1, 0, 0, 1, -1, 1, -1};
 	
-	public OthelloGame(Player player1, Player player2) {
-		board = new OthelloBoard();
+	public OthelloGame(Player player1, Player player2, Board board) {
+		this.board = board;
 		players = new ArrayList<Player>();
 		players.add(player1);
 		players.add(player2);
@@ -27,40 +32,99 @@ public class OthelloGame implements Othello{
 
 	@Override
 	public List<Node> getNodesToSwap(String playerId, String nodeId) {
-		// TODO Auto-generated method stub
-		return null;
+		List<Node> swappedNodes = new ArrayList<Node>();
+		Node move = null;//TODO Bryt ut till en ny metod
+		for(Node node : board.getNodes()) {
+			if(node.getId().equals(nodeId))
+				move = node;
+		}
+		if(move == null) 
+			return null;
+		
+		for(int i = 0; i < dY.length; i++) {
+			List<Node> nodesInDirection = checkSwapDirection(playerId, move, dX[i], dY[i]);
+			if(nodesInDirection != null)
+				swappedNodes.addAll(nodesInDirection);			
+		}
+		return swappedNodes;
 	}
 
 	@Override
 	public Player getPlayerInTurn() {
-		// TODO Auto-generated method stub
-		return null;
+		return playerInTurn;
 	}
 
 	@Override
 	public List<Player> getPlayers() {
-		// TODO Auto-generated method stub
-		return null;
+		return players;
 	}
 
 	@Override
 	public boolean hasValidMove(String playerId) {
-		// TODO Auto-generated method stub
-		return false;
+		if(findPossibleMoves(playerId).size() == 0)
+			return false;
+		return true;
 	}
 
 	@Override
 	public boolean isActive() {
-		// TODO Auto-generated method stub
-		return false;
+		return active;
 	}
 
 	@Override
 	public boolean isMoveValid(String playerId, String nodeId) {
-		// TODO Auto-generated method stub
+		Node move = null;
+		for(Node node : board.getNodes()) {
+			if(node.getId().equals(nodeId))
+				move = node;
+		}
+		if(move == null) 
+			return false;
+		
+		for(int i = 0; i < dY.length; i++) {
+			if(checkDirection(playerId, move, dX[i], dY[i]))
+				return true;
+		}
 		return false;
 	}
+	
+	private boolean checkDirection(String playerId, Node move, int xDir, int yDir) {
+		Node next = getNode(move.getXCoordinate()+xDir, move.getYCoordinate()+yDir);
+		if(!next.isMarked() || next.getOccupantPlayerId().equals(playerId))
+			return false;
+		do {
+			next = getNode(next.getXCoordinate()+xDir, next.getYCoordinate()+yDir);
+			if(next == null)
+				break;
+			if(next.getOccupantPlayerId().equals(playerId)) {
+				return true;
+			}
 
+		} while(next.isMarked());
+		return false;
+	}
+	
+	private List<Node> checkSwapDirection(String playerId, Node move, int xDir, int yDir) {
+			Node next = getNode(move.getXCoordinate()+xDir, move.getYCoordinate()+yDir);
+			if(!next.isMarked() || next.getOccupantPlayerId().equals(playerId))
+				return null;
+			List<Node> swappedNodes = new ArrayList<Node>();
+			while(!next.getOccupantPlayerId().equals(playerId)) {
+				swappedNodes.add(next);
+				next = getNode(next.getXCoordinate()+xDir, next.getYCoordinate()+yDir);
+				if(next == null || !next.isMarked())
+					return null;
+			} 
+			return swappedNodes;
+	}
+	
+	private Node getNode(int x, int y) {
+		int index = 8*y+x;
+		if(index > board.getNodes().size() || index < 0)
+			return null;
+		return board.getNodes().get(index);
+	}
+	
 	@Override
 	public List<Node> move() {
 		// TODO Auto-generated method stub
@@ -75,14 +139,38 @@ public class OthelloGame implements Othello{
 
 	@Override
 	public void start() {
-		// TODO Auto-generated method stub
-		
+		Random random = new Random();
+		int player = random.nextInt(2);
+		playerInTurn = players.get(player);
+		active = true;
 	}
 
 	@Override
 	public void start(String playerId) {
-		// TODO Auto-generated method stub
-		
+		playerInTurn = getPlayer(playerId);
+		active = true;
+	}
+
+	private Player getPlayer(String playerId) {
+		if(players.get(0).getId().equals(playerId)) {
+			return players.get(0);
+		} else if (players.get(1).getId().equals(playerId)) {
+			return players.get(1);
+		} else {
+			return null;
+		}
+	}
+	
+	private List<Node> findPossibleMoves(String playerId) {
+		List<Node> moves = new ArrayList<Node>();
+		for(Node node : board.getNodes()) {
+			if(!node.isMarked()) {
+				if(isMoveValid(playerId, node.getId())) {
+					moves.add(node);
+				}
+			}
+		}
+		return moves;
 	}
 
 }
--- src/kth/game/othello/board/OthelloBoard.java
+++ src/kth/game/othello/board/OthelloBoard.java
@@ -6,25 +6,9 @@ import java.util.List;
 public class OthelloBoard implements Board{
 	
 	private List<Node> nodes;
-	private static final int ROWS = 8;
-	private static final int COLUMNS = 8;
-
 	
 	public OthelloBoard() {
 		nodes = new ArrayList<Node>();
-		createNodes();
-	}
-	
-	/**
-	 * Fill the board with nodes.
-	 */
-	private void createNodes() {
-		for(int row = 0; row < ROWS; row++)  {
-			for(int column = 0; column < COLUMNS; column++) {
-				String id = Integer.toString((COLUMNS*row+column));
-				nodes.add(new OthelloNode(column, row, false, id, null));
-			}
-		}
 	}
 	
 	@Override
