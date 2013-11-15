bc1e4c98cad59597792a4b7dcc634c694e32c06c
AntonLovmar <antonlovmar@gmail.com>
2013-11-15 15:14:17 +0100

    Fixed a case in demo2 where the player could possibly try to move even if it was not his turn

---
+++ src/kth/game/othello/tests/OthelloLab1IT.java
@@ -0,0 +1,82 @@
+package kth.game.othello.tests;
+
+import java.util.List;
+
+import junit.framework.Assert;
+import kth.game.othello.Othello;
+import kth.game.othello.OthelloFactory;
+import kth.game.othello.OthelloFactoryImpl;
+import kth.game.othello.board.Node;
+import kth.game.othello.player.Player;
+import kth.game.othello.player.Player.Type;
+
+import org.junit.Test;
+
+public class OthelloLab1IT {
+
+	private Object getNumberOfOccupiedNodes(Othello othello) {
+		int occupiedNodesCounter = 0;
+		for (Node node : othello.getBoard().getNodes()) {
+			if (node.isMarked()) {
+				occupiedNodesCounter++;
+			}
+		}
+		return occupiedNodesCounter;
+	}
+
+	private OthelloFactory getOthelloFactory() {
+		return new OthelloFactoryImpl();
+	}
+
+	private void makeAHumanMove(Othello othello, Player human) {
+		for (Node node : othello.getBoard().getNodes()) {
+			if (othello.isMoveValid(human.getId(), node.getId())) {
+				othello.move(human.getId(), node.getId());
+				return;
+			}
+		}
+		throw new IllegalStateException();
+	}
+
+	@Test
+	public void someMovesBetweenAComputerAndHumanTest() {
+		Othello othello = getOthelloFactory().createHumanVersusComputerGameOnOriginalBoard();
+		Player human = null;
+		if (othello.getPlayers().get(0).getType() == Type.COMPUTER) {
+			human = othello.getPlayers().get(1);
+		} else {
+			human = othello.getPlayers().get(0);
+		}
+		othello.start(human.getId());
+		OthelloImplTest.printBoard(othello);
+		Assert.assertEquals(4, getNumberOfOccupiedNodes(othello));
+		makeAHumanMove(othello, human);
+		OthelloImplTest.printBoard(othello);
+		Assert.assertEquals(5, getNumberOfOccupiedNodes(othello));
+		othello.move();
+		OthelloImplTest.printBoard(othello);
+		Assert.assertEquals(6, getNumberOfOccupiedNodes(othello));
+		Assert.assertEquals(human.getId(), othello.getPlayerInTurn().getId());
+		Assert.assertTrue(othello.hasValidMove(human.getId()));
+		makeAHumanMove(othello, human);
+		OthelloImplTest.printBoard(othello);
+		Assert.assertEquals(7, getNumberOfOccupiedNodes(othello)); //fail
+		othello.move();
+		Assert.assertEquals(8, getNumberOfOccupiedNodes(othello));
+		makeAHumanMove(othello, human);
+		Assert.assertEquals(9, getNumberOfOccupiedNodes(othello));
+		othello.move();
+		Assert.assertEquals(10, getNumberOfOccupiedNodes(othello));
+	}
+
+	@Test
+	public void twoComputerOnAClassicalBoardTest() {
+		Othello othello = getOthelloFactory().createComputerGameOnClassicalBoard();
+		othello.start(othello.getPlayers().get(0).getId());
+		while (othello.isActive()) {
+			Assert.assertEquals(Type.COMPUTER, othello.getPlayerInTurn().getType());
+			othello.move();
+		}
+	}
+
+}
