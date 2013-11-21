a410ab395e92d15147f99f166802f5c80e6ba8e7
tobiasandersson <tobias.ason@gmail.com>
2013-11-21 16:35:37 +0100

    added tests for RuleHandler

--- src/kth/game/othello/tests/RuleHandlerTest.java
+++ src/kth/game/othello/tests/RuleHandlerTest.java
@@ -6,10 +6,15 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import kth.game.othello.BoardHandler;
 import kth.game.othello.PlayerHandler;
 import kth.game.othello.RuleHandler;
 import kth.game.othello.board.Node;
+import kth.game.othello.player.Player;
 
 import org.junit.Test;
 
@@ -62,6 +67,90 @@ public class RuleHandlerTest {
 	}
 
 	@Test
+	public void testHasValidMove() {
+		BoardHandler bh = mock(BoardHandler.class);
+		PlayerHandler ph = mock(PlayerHandler.class);
+		RuleHandler rh = new RuleHandler(bh, ph);
+
+		Node moveTo = mock(Node.class);
+		when(moveTo.getXCoordinate()).thenReturn(1);
+		when(moveTo.getYCoordinate()).thenReturn(1);
+		when(moveTo.isMarked()).thenReturn(false);
+		when(moveTo.getOccupantPlayerId()).thenReturn(null);
+
+		Node node2 = mock(Node.class);
+		when(node2.getXCoordinate()).thenReturn(1);
+		when(node2.getYCoordinate()).thenReturn(2);
+		when(node2.isMarked()).thenReturn(true);
+		when(node2.getOccupantPlayerId()).thenReturn("player2");
+
+		Node node3 = mock(Node.class);
+		when(node3.getXCoordinate()).thenReturn(1);
+		when(node3.getYCoordinate()).thenReturn(3);
+		when(node3.isMarked()).thenReturn(true);
+		when(node3.getOccupantPlayerId()).thenReturn("player1");
+
+		when(bh.getNode(anyString())).thenReturn(moveTo);
+		when(bh.getNode(1, 2)).thenReturn(node2);
+		when(bh.getNode(1, 3)).thenReturn(node3);
+
+		List<Node> nodeList = new ArrayList<Node>();
+		nodeList.add(moveTo);
+		nodeList.add(node2);
+		nodeList.add(node3);
+
+		when(bh.getNodes()).thenReturn(nodeList);
+		assertTrue(rh.hasValidMove("player1"));
+		assertFalse(rh.hasValidMove("player2"));
+	}
+
+	@Test
+	public void testIsActive() {
+		BoardHandler bh = mock(BoardHandler.class);
+		PlayerHandler ph = mock(PlayerHandler.class);
+		RuleHandler rh = new RuleHandler(bh, ph);
+
+		Node moveTo = mock(Node.class);
+		when(moveTo.getXCoordinate()).thenReturn(1);
+		when(moveTo.getYCoordinate()).thenReturn(1);
+		when(moveTo.isMarked()).thenReturn(false);
+		when(moveTo.getOccupantPlayerId()).thenReturn(null);
+
+		Node node2 = mock(Node.class);
+		when(node2.getXCoordinate()).thenReturn(1);
+		when(node2.getYCoordinate()).thenReturn(2);
+		when(node2.isMarked()).thenReturn(true);
+		when(node2.getOccupantPlayerId()).thenReturn("player2");
+
+		Node node3 = mock(Node.class);
+		when(node3.getXCoordinate()).thenReturn(1);
+		when(node3.getYCoordinate()).thenReturn(3);
+		when(node3.isMarked()).thenReturn(true);
+		when(node3.getOccupantPlayerId()).thenReturn("player1");
+
+		when(bh.getNode(anyString())).thenReturn(moveTo);
+		when(bh.getNode(1, 2)).thenReturn(node2);
+		when(bh.getNode(1, 3)).thenReturn(node3);
+
+		List<Node> nodeList = new ArrayList<Node>();
+		nodeList.add(moveTo);
+		nodeList.add(node2);
+		nodeList.add(node3);
+
+		when(bh.getNodes()).thenReturn(nodeList);
+		List<Player> players = new ArrayList<Player>();
+		Player p1 = mock(Player.class);
+		when(p1.getId()).thenReturn("player1");
+		Player p2 = mock(Player.class);
+		when(p2.getId()).thenReturn("player2");
+		players.add(p1);
+		players.add(p2);
+		when(ph.getPlayers()).thenReturn(players);
+
+		assertTrue(rh.isActive());
+	}
+
+	@Test
 	public void testGetNodesToSwap() {
 		BoardHandler bh = mock(BoardHandler.class);
 		PlayerHandler ph = mock(PlayerHandler.class);
@@ -94,4 +183,5 @@ public class RuleHandlerTest {
 
 		assertEquals(1, rh.getNodesToSwap("player1", moveTo.getId()).size());
 	}
+
 }
