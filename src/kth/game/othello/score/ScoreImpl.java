dbb728a575cca8b85a0c56226c7cba046512a4d9
AntonLovmar <antonlovmar@gmail.com>
2013-11-25 15:24:43 +0100

    Added untracked CreatorImpls

---
+++ src/kth/game/othello/score/ScoreImpl.java
@@ -0,0 +1,25 @@
+package kth.game.othello.score;
+
+import java.util.List;
+import java.util.Observer;
+
+public class ScoreImpl implements Score{
+
+	@Override
+	public void addObserver(Observer observer) {
+		// TODO Auto-generated method stub	
+	}
+
+	@Override
+	public List<ScoreItem> getPlayersScore() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getPoints(String playerId) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
