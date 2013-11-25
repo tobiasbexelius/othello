70c44952236ce502007afb06bee418a913a7aa0e
tobiasandersson <tobias.ason@gmail.com>
2013-11-25 15:47:14 +0100

    Added MoveStrategyTest

---
+++ src/kth/game/othello/tests/MoveStrategyTest.java
@@ -0,0 +1,22 @@
+package kth.game.othello.tests;
+
+import org.junit.Test;
+
+public class MoveStrategyTest {
+
+	@Test
+	public void testRandomMoveStrategy() {
+
+	}
+
+	@Test
+	public void testFirstMoveStrategy() {
+
+	}
+
+	@Test
+	public void testGreedyMoveStrategy() {
+
+	}
+
+}
