c3d89342955f26622066171cd7e28e4f0b1c28ee
AntonLovmar <antonlovmar@gmail.com>
2013-11-25 16:16:01 +0100

    Implemented ScoreImpl

--- src/kth/game/othello/score/ScoreImpl.java
+++ src/kth/game/othello/score/ScoreImpl.java
@@ -1,32 +1,50 @@
 package kth.game.othello.score;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Observable;
 import java.util.Observer;
 
-public class ScoreImpl implements Score, Observer{
+import kth.game.othello.board.Node;
 
-	@Override
-	public void addObserver(Observer observer) {
-		// TODO Auto-generated method stub	
+public class ScoreImpl extends Observable implements Score, Observer{
+
+	private List<ScoreItem> playersScores;
+	
+	public ScoreImpl() {
+		playersScores = new ArrayList<ScoreItem>();
 	}
-
+	
 	@Override
 	public List<ScoreItem> getPlayersScore() {
-		// TODO Auto-generated method stub
-		return null;
+		Collections.sort(playersScores);
+		return playersScores;
 	}
 
 	@Override
 	public int getPoints(String playerId) {
-		// TODO Auto-generated method stub
-		return 0;
+		for(ScoreItem item : playersScores) {
+			if(item.getPlayerId().equals(playerId)) {
+				return item.getScore();
+			}
+		}
+		return -1;
 	}
 
 	@Override
 	public void update(Observable o, Object arg) {
-		// TODO Auto-generated method stub
-		
+		Node node = (Node) o;
+		String oldOccupant = (String) arg;
+		String newOccupant = node.getOccupantPlayerId();
+		for(ScoreItem item : playersScores) {
+			if(item.getPlayerId().equals(oldOccupant)) {
+				item.decrementScore();
+			}
+			if(item.getPlayerId().equals(newOccupant)) {
+				item.incrementScore();
+			}
+		}
 	}
 
 }
