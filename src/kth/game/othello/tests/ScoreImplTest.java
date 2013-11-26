ab33b13eff04fe41d6abee22b893547d282b6636
AntonLovmar <antonlovmar@gmail.com>
2013-11-26 16:49:18 +0100

    Finished tests for ScoreImpl

--- src/kth/game/othello/tests/ScoreImplTest.java
+++ src/kth/game/othello/tests/ScoreImplTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Observer;
 
 import kth.game.othello.board.Board;
 import kth.game.othello.board.BoardImpl;
@@ -78,7 +79,96 @@ public class ScoreImplTest {
 		assertEquals(2, score.getPoints("player1"));
 		assertEquals(1, score.getPoints("player2"));
 		assertEquals(0, score.getPoints("player3"));
-
 	}
+	
+	@Test
+	public void getPointsTest() {
+		List<Player> players = new ArrayList<Player>();
+		Player player1 = Mockito.mock(PlayerImpl.class);
+		Mockito.when(player1.getId()).thenReturn("player1");
+		Player player2 = Mockito.mock(PlayerImpl.class);
+		Mockito.when(player2.getId()).thenReturn("player2");
+		Player player3 = Mockito.mock(PlayerImpl.class);
+		Mockito.when(player3.getId()).thenReturn("player3");
+		players.add(player1);
+		players.add(player2);
+		players.add(player3);
+		
+		NodeImpl node = Mockito.mock(NodeImpl.class);
+		Mockito.when(node.getOccupantPlayerId()).thenReturn("player1");
+		
+		ScoreImpl score = new ScoreImpl(players);
+		assertEquals(0, score.getPoints("player1"));
+		assertEquals(0, score.getPoints("player2"));
+		assertEquals(0, score.getPoints("player3"));
+		
+		score.update(node, null);
+		assertEquals(1, score.getPoints("player1"));
+		score.update(node, null);
+		assertEquals(2, score.getPoints("player1"));
+		
+		Mockito.when(node.getOccupantPlayerId()).thenReturn("player2");
+		score.update(node, null);
+		score.update(node, null);
+		score.update(node, null);
+		score.update(node, null);
+		assertEquals(4, score.getPoints("player2"));
+		assertEquals(2, score.getPoints("player1"));
+		assertEquals(0, score.getPoints("player3"));
+	}
+	
+	@Test
+	public void getPlayerScoresTest() {
+		List<Player> players = new ArrayList<Player>();
+		Player player1 = Mockito.mock(PlayerImpl.class);
+		Mockito.when(player1.getId()).thenReturn("player1");
+		Player player2 = Mockito.mock(PlayerImpl.class);
+		Mockito.when(player2.getId()).thenReturn("player2");
+		Player player3 = Mockito.mock(PlayerImpl.class);
+		Mockito.when(player3.getId()).thenReturn("player3");
+		players.add(player1);
+		players.add(player2);
+		players.add(player3);
+			
+		ScoreImpl score = new ScoreImpl(players);
+		assertEquals(3, score.getPlayersScore().size());
+		
+		Player player4 = Mockito.mock(PlayerImpl.class);
+		Mockito.when(player1.getId()).thenReturn("player4");
+		Player player5 = Mockito.mock(PlayerImpl.class);
+		Mockito.when(player2.getId()).thenReturn("player5");
+		Player player6 = Mockito.mock(PlayerImpl.class);
+		Mockito.when(player3.getId()).thenReturn("player6");
+		players.add(player4);
+		players.add(player5);
+		players.add(player6);
+		
+		score = new ScoreImpl(players);
+		assertEquals(6, score.getPlayersScore().size());
+	}
+	
+	@Test
+	public void updateAndNotifyTest() {
+		List<Player> players = new ArrayList<Player>();
+		Player player1 = Mockito.mock(PlayerImpl.class);
+		Mockito.when(player1.getId()).thenReturn("player1");
+		Player player2 = Mockito.mock(PlayerImpl.class);
+		Mockito.when(player2.getId()).thenReturn("player2");
+		players.add(player1);
+		players.add(player2);
+		
+		Observer observer = Mockito.mock(Observer.class);
+		
+		NodeImpl node = Mockito.mock(NodeImpl.class);
+		Mockito.when(node.getOccupantPlayerId()).thenReturn("player1");
+		
+		ScoreImpl score = new ScoreImpl(players);
+		assertEquals(0, score.getPoints("player1"));
+		score.addObserver(observer);
+		score.update(node, null);
+		assertEquals(1, score.getPoints("player1"));
+		Mockito.verify(observer).update(score, null);
+	}
+	
 
 }
